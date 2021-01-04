6e5101f40e3ccfc12a5a61cbe042e953d2a93acd
K-weber-designs <72924250+K-weber-designs@users.noreply.github.com>
2021-01-04 11:39:40 +0100

    Task 2.3 JS Libraries & Plugins
    changes made to menu toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log ("Welcome to my World")
+function menuToggle ()
+{
+var x = document.getElementById('myNavtoggle');
+       if(x.className==='navtoggle'){x.className += 'responsive';}
+      else {x.className = 'myNavtoggle'}
+}
